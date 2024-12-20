313c38a570d4aa2fc52d11f269da89f5c5624cc9
kadanarpa <kadanarpa@gmail.com>
2024-12-20 13:19:10 -0500

    feat: render-markdown.nvim

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"echasnovski/mini.nvim",
+		},
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {
+			heading = {
+				left_pad = 1,
+			},
+		},
+	},
+}
