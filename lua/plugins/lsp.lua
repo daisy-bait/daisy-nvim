c8fab78f55a3cda8e0626e6263ad60bb7adde513
kadanarpa <kadanarpa@gmail.com>
2024-12-25 14:29:40 -0500

    feat: lsp.lua keymaps for format and diagnostics
    - basic autocomplete with Ctrl + space

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,14 +5,30 @@ return {
     "folke/neodev.nvim",
   },
   config = function()
+    --- Global keybindings
+    ---
+    --- Diagnostics
+    vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+    vim.keymap.set('n', '<space>op', vim.diagnostic.setloclist)
 
+    --- For specific servers, pass it intro setup list
     local on_attach = function(_, bufnr)
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer = bufnr})
+      --- Enabling autocomplete
+      vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+      vim.keymap.set('i', '<C-space>', '<C-x><C-o>', { noremap = true, silent = true })
+
+      --- Formatting
+      vim.keymap.set('n', '<space>ft', vim.lsp.buf.format)
+
+      --- By default, this option is enabled anyway
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr })
     end
 
     require("neodev").setup()
     local lsp = require("lspconfig")
     lsp.lua_ls.setup({ on_attach = on_attach })
-    lsp.bashls.setup({})
+    lsp.bashls.setup({ on_attach = on_attach })
   end
 }
